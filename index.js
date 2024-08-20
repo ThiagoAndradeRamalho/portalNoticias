cfcbb2001cf3e6c7f2f95dd8bb7b25ae13c68169
Thiaago79 <thiandrade79@gmail.com>
2024-08-19 22:58:10 -0300

    Subindo parte do front

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
-const express = require('express')
-const http = require('http')
+const express = require('express');
+const bodyParser = require('body-parser')
+const path = require('path');
+const app = express();
 
-var tarefas = ['olhar','mover']
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
 
-var app = express()
+app.engine('html',require('ejs').renderFile);
+app.set('view engine', 'html');
+app.use('/public', express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, '/pages'));
 
 app.get('/',(req,res)=>{
-    res.send('html',{tarefasList:tarefas})
+
+    console.log(req.query);
+
+    if(req.query.busca == null){
+        res.render('home',{})
+    }else{
+        res.send('Voce buscou: ' + req.query.busca)
+    }
+    
 })
 
-app.listen(5000, function(){
-    console.log("Server rodando")
+app.get('/:slug',(req,res)=>{
+    res.send(req.params.slug)
+})
+
+app.listen(5000,()=>{
+    console.log('Server rodando');
 })
