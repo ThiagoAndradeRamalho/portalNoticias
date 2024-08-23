cdd8239c0b17ebd5e6a5028d570593a05950abfe
Thiago Andrade <thiandrade79@gmail.com>
2024-08-23 17:46:45 -0300

    Adicionando conexao com MongoDB

--- index.js
+++ index.js
@@ -1,42 +1,40 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const mogoose = require('mongoose');
+const mongoose = require('mongoose');
 const path = require('path');
 const app = express();
 
-mogoose.connect('',{useNewUrlParser: true, uneUnifiedTopology: true}).then(function(){
-    console.log("Conectado com sucesso")
-}).catch(function(err){
-    console.log(err.message)
-})
+mongoose.connect('mongodb+srv://root:ZUGEnnzA9Lby9em5@cluster0.ogzvt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',{
+}).then(function() {
+    console.log("Conectado com sucesso");
+}).catch(function(err) {
+    console.log(err.message);
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-app.engine('html',require('ejs').renderFile);
+app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 app.use('/public', express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, '/pages'));
 
-app.get('/',(req,res)=>{
-
+app.get('/', (req, res) => {
     console.log(req.query);
 
-    if(req.query.busca == null){
-        res.render('home',{})
-    }else{
-        res.render('busca',{})
+    if (req.query.busca == null) {
+        res.render('home', {})
+    } else {
+        res.render('busca', {})
     }
-    
-})
+});
 
-app.get('/:slug',(req,res)=>{
-    //res.send(req.params.slug)
-    res.render('single',{})
-})
+app.get('/:slug', (req, res) => {
+    res.render('single', {})
+});
 
-app.listen(5000,()=>{
+app.listen(5000, () => {
     console.log('Server rodando');
-})
+});
